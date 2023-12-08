5c76cb4007984ecba687d290e815ee212f4f4019
Antonio Perez <antonio.perez@gametime.co>
2023-12-08 14:40:33 -0600

    Match example3.txt test data file result

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "log"
     "os"
     "regexp"
+    "strings"
 )
 
 const (
@@ -15,10 +16,15 @@ const (
     finalNode = "ZZZ"
 )
 
+var (
+    startNodes []string
+    finalNodes []string
+)
+
 type Network map[string]map[rune]string
 
 func main() {
-    pf, err := os.Open("example2.txt")
+    pf, err := os.Open("example3.txt")
     if err != nil {
         log.Fatalf("while opening file %q: %s", pf.Name(), err)
     }
@@ -28,8 +34,8 @@ func main() {
 
     var instr string
     network := make(Network)
-    reSteps := regexp.MustCompile(`\s*([LR]+)\s*`)
-    reNode := regexp.MustCompile(`([A-Z]{3})\s=\s\(([A-Z]{3}),\s([A-Z]{3})\)`)
+    reSteps := regexp.MustCompile(`^\s*([LR]+)\s*$`)
+    reNode := regexp.MustCompile(`([A-Z1-9]{3})\s=\s\(([A-Z1-9]{3}),\s([A-Z1-9]{3})\)`)
 
     for scnr.Scan() {
         line := scnr.Text()
@@ -41,19 +47,39 @@ func main() {
             network[nodes[1]] = make(map[rune]string)
             network[nodes[1]][Left] = nodes[2]
             network[nodes[1]][Right] = nodes[3]
+            if strings.HasSuffix(nodes[1], "A") {
+                startNodes = append(startNodes, nodes[1])
+            }
+            if strings.HasSuffix(nodes[1], "Z") {
+                finalNodes = append(finalNodes, nodes[1])
+            }
         }
     }
 
     steps := 0
-    nextNode := startNode
+    // nextNode := startNode
+    nextNodes := make([]string, 0)
+
+    nextNodes = append(nextNodes, startNodes...)
 
     for {
         for _, step := range instr {
-            fmt.Println(nextNode)
+            finish := true
+            fmt.Println(nextNodes)
             steps++
-            nextNode = network[nextNode][step]
-            if nextNode == finalNode {
-                fmt.Println(nextNode)
+            for idx, nextNode := range nextNodes {
+                nextNodes[idx] = network[nextNode][step]
+            }
+
+            for _, nextNode := range nextNodes {
+                if !strings.HasSuffix(nextNode, "Z") {
+                    finish = false
+                    break
+                }
+            }
+
+            if finish {
+                fmt.Println(nextNodes)
                 fmt.Println(steps)
                 return
             }
